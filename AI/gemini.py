ae5a946af107d7449bab564679ddf95206562273
tarimisu <pdpvault@gmail.com>
2025-01-28 15:34:39 -0500

    Remove Gemini from Public

--- AI/gemini.py
+++
@@ -1,6 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyCxlyyVQFb4ZOAi0LdGrYu3OnRdoGsaP3k")
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Can you understand a pandas dataframe")
-print(response.text)
